0b92ce6746ad931be27e480d2fb4ebbe5bc2af37
Nick Vergevsky <vergevsky@gmail.com>
2014-12-26 20:19:05 +0500

    Create profile.php

---
+++ profile.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Gregory Boyle</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+	<header>
+		<div class="width">
+			<div id="logo">Ruvio</div>
+			<div id="topnav">
+				<img src="img/subscribe.png">
+				<img src="img/question.png">
+				<img src="img/news.png">
+				<img src="img/search.png">
+			</div>
+			<div id="user">Gregory Boyle</div>
+		</div>	
+	</header>
+</body>
+</html>
